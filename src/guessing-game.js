a22f824fbd3aa648a1141a8d84ea392a054edacf
niowa <andres7480311@mail.ru>
2017-01-29 15:59:35 +0300

    git init

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
     }
 
     guess() {
-
+		this.med = Math.round((this.min + this.max) / 2);
+		return this.med;
     }
 
     lower() {
-
+		this.max = this.med;
     }
 
     greater() {
-
+		this.min = this.med;
     }
 }
 
